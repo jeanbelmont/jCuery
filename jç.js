94053ab481237658eb65740ad1001504f925c70d
Jean Belmont Dantes <39181678+jeanbelmont@users.noreply.github.com>
2021-02-09 00:47:24 -0600

    Mi primer versión de jÇuery
    Pequeña librería JavaScript que uso en todos mis proyectos personales, hago uso de ECMAScript 2018

---
+++ jç.js
@@ -0,0 +1,9 @@
+/*!
+ * jÇuery JavaScript Library v0.0.1
+ * https://github.com/jeanbelmont/jCuery
+ * ES version 2018
+ * Çopyright ç
+ * Sin licencia para usarse sin mi permiso
+ *
+ * Date: 2021-09-02T06:41z
+ */
